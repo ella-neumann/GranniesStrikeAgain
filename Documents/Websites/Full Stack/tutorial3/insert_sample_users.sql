17d62999874c4a8d114240a95931add1505f271e
Jessica-Stillwell <46616341+Jessica-Stillwell@users.noreply.github.com>
2021-02-06 19:50:36 -0800

    Delete Documents/Websites/Full Stack directory

--- Documents/Websites/Full Stack/tutorial3/insert_sample_users.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users ([handle], [name], [bio])
-VALUES
-    ('Emma Watson', 'greenThumb', 'Hey, I am Emma! When I am not filming Harry Potter I am planting!'),
-    ('Anice','aNiceFarmer','I love growing vegetables, so I can eat them fresh!'),
-    ('Gerald', 'geraldGrows', 'Hey! If you have bamboo let me know. I am obsessed with bamboo!')
--- Documents/Websites/Full Stack/tutorial3/user.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-    [handle] VARCHAR(50),
-    [name] VARCHAR(50),
-    [bio] TEXT
-);
-
