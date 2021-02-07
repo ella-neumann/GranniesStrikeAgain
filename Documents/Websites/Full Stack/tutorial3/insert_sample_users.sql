22e248dc238f353d9030bb2fb241b63fd3c509db
Jessica-Stillwell <46616341+Jessica-Stillwell@users.noreply.github.com>
2021-02-06 19:49:24 -0800

    Added tut2/3/4 assignment information

---
+++ Documents/Websites/Full Stack/tutorial3/insert_sample_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users ([handle], [name], [bio])
+VALUES
+    ('Emma Watson', 'greenThumb', 'Hey, I am Emma! When I am not filming Harry Potter I am planting!'),
+    ('Anice','aNiceFarmer','I love growing vegetables, so I can eat them fresh!'),
+    ('Gerald', 'geraldGrows', 'Hey! If you have bamboo let me know. I am obsessed with bamboo!')
---
+++ Documents/Websites/Full Stack/tutorial3/user.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    [handle] VARCHAR(50),
+    [name] VARCHAR(50),
+    [bio] TEXT
+);
+
